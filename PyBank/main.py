145264c6231056293e0d19c2f40a4ff3a99545ed
DerekLarson77 <DerekLarson77@gmail.com>
2021-04-07 11:40:40 -0700

    Text file function complete
    Function to write Financial Analysis complete.

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,14 +3,20 @@ import os
 # Module for reading CSV file.
 import csv
 
-def file_output(total_months, net_total):
+# Function that looks for requires our 5 calculations and then writes those answers to a text file.
+def file_output(total_months, net_total, net_changes, great_profit, great_loss):
 
+# The text file location starts where this main.py is located and then inside folder 'analysis' with the file name 'results'.
     output_path = os.path.join("analysis", "results.txt")
 
     with open(output_path, 'w', newline='') as txtfile:
-
-        txtfile.write(str(total_months))
-        txtfile.write(str(net_total))
+        txtfile.write("Financial Analysis")
+        txtfile.write('\n' + "---------------------------------")
+        txtfile.write('\n' + "Total Months:  " + str(total_months))
+        txtfile.write('\n' + "Total:  $" + str(net_total))
+        txtfile.write('\n' + "Average Change:  $" + str(net_changes))
+        txtfile.write('\n' + "Greatest Increase in Profits:  " + str(great_profit))
+        txtfile.write('\n' + "Greatest Decrease in Profits:  " + str(great_loss))
 
 # The CSV file path starts where this main.py is located and then inside folder 'Resources' with file name 'budget_data'.
 csvpath = os.path.join("Resources", "budget_data.csv")
@@ -28,14 +34,20 @@ with open(csvpath) as csvfile:
     # Setting counting variables to zero before any math occurs.
     total_months = 0
     net_total = 0
+    net_changes = 0
+    greatest_increase = 0
+    greatest_decrease = 0
 
     # Read each row in the CSV file.
     for row in csvreader:
         total_months += 1
         net_total += int(row[1])
-
+print("")
+print("Financial Analysis")
+print("---------------------------------")
 print(f"Total Months:  {total_months}")
 print(f"Total:  {net_total}")
+print("")
 
-file_output(total_months, net_total)
+file_output(total_months, net_total, net_changes, greatest_increase, greatest_decrease)
 
