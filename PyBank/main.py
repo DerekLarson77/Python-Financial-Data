d17c319603701d053cd815b7ad3e7a47e974146d
DerekLarson77 <DerekLarson77@gmail.com>
2021-04-07 12:41:10 -0700

    Average Change added but NOT accurate
    Average change prints to Terminal and writes to Text file.
    
    Need to fix calculations still.

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,7 @@ import os
 import csv
 
 # Function that looks for requires our 5 calculations and then writes those answers to a text file.
-def file_output(total_months, net_total, net_changes, great_profit, great_loss):
+def file_output(total_months, net_total, avg_changes, great_profit, great_loss):
 
 # The text file location starts where this main.py is located and then inside folder 'analysis' with the file name 'results'.
     output_path = os.path.join("analysis", "results.txt")
@@ -15,7 +15,7 @@ def file_output(total_months, net_total, net_changes, great_profit, great_loss):
         txtfile.write('\n' + "---------------------------------")
         txtfile.write('\n' + "Total Months:  " + str(total_months))
         txtfile.write('\n' + "Total:  $" + str(net_total))
-        txtfile.write('\n' + "Average Change:  $" + str(net_changes))
+        txtfile.write('\n' + "Average Change:  $" + str(avg_changes))
         txtfile.write('\n' + "Greatest Increase in Profits:  " + str(great_profit))
         txtfile.write('\n' + "Greatest Decrease in Profits:  " + str(great_loss))
 
@@ -34,17 +34,19 @@ with open(csvpath) as csvfile:
 
     # Setting counting variables to zero before any math occurs.
     total_months = 0
+    net_total = 0
 
-    ## Variables for index[0] and index[1] of the current row and the previous row
+    ## Variables for index[0] and index[1] of the current row and the previous row.
     net_previous = 0
     net_current = 0
     month_previous = ""
     month_current = ""
 
-    net_total = 0
+    ## Variables for calculating change in profit/loss and then adding up the total.
+    change_current = 0
     net_changes = 0
 
-    ## Variables for month and profit/loss of both largest increase and decrease
+    ## Variables for month and profit/loss of both largest increase and decrease.
     greatest_net_increase = 0
     greatest_month_increase = ""
     greatest_net_decrease = 0
@@ -64,18 +66,23 @@ with open(csvpath) as csvfile:
         # Update month_current to the current row value.
         month_current = row[0]
 
-        if((net_current - net_previous) > greatest_net_increase):
-            greatest_net_increase = net_current - net_previous
-            greatest_month_increase = month_current
-        elif((net_current - net_previous) < greatest_net_decrease):
-            greatest_net_decrease = net_current - net_previous
-            greatest_month_decrease = month_current
+        change_current = net_current - net_previous
 
+        if(change_current > greatest_net_increase):
+            greatest_net_increase = change_current
+            greatest_month_increase = month_current
+        elif(change_current < greatest_net_decrease):
+            greatest_net_decrease = change_current
+            greatest_month_decrease = month_current
 
         # Continue to add up the net values of each row.
         net_total += net_current
+        net_changes += change_current
 
-# Variable for cleaner print statements and to match format needed for function (file_output)
+# Calculating the average change by taking the total change divided by the total months.
+average_change = net_changes / total_months
+
+# Variable for cleaner print statements and to match format needed for function (file_output).
 greatest_increase = greatest_month_increase + " ($" + str(greatest_net_increase) + ")"
 greatest_decrease = greatest_month_decrease + " ($" + str(greatest_net_decrease) + ")"
 
@@ -85,11 +92,11 @@ print("Financial Analysis")
 print("---------------------------------")
 print(f"Total Months:  {total_months}")
 print(f"Total:  {net_total}")
-print("")
+print("Average Change:  $" + str(average_change))
 print("Greatest Increase in Profits:  " + greatest_increase)
 print("Greatest Decrease in Profits:  " + greatest_decrease)
 print("")
 
 
-file_output(total_months, net_total, net_changes, greatest_increase, greatest_decrease)
+file_output(total_months, net_total, average_change, greatest_increase, greatest_decrease)
 
